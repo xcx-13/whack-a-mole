bd4533360342b9de8bfccf2dac15c7fa12bc4639
Ece <ece.2002.67@gmail.com>
2024-11-17 19:14:42 +0100

    wip

--- script.js
+++ script.js
@@ -1,55 +1,124 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+class Game {
+    slots;
+    score = 0;
+    time = 60;
+    count;
+    timerId;
+    started = false;
+    position;
 
-let result = 0
-let hitPosition
-let currentTime = 60
-let timerId = null
-let milliSecond = 10;
-
-function randomSquare() {
-    squares.forEach(square => {
-        square.classList.remove('mole')
-    })
-    let randomSquarePosition = squares[Math.floor(Math.random() * 9)]
-    randomSquarePosition.classList.add('mole')
-
-    hitPosition = randomSquarePosition.id
+    constructor(slots) {
+        this.slots = slots;
+    }
 }
 
-squares.forEach(square => {
-    square.addEventListener('mousedown', () => {
-        if (square.id == hitPosition) {
-            result++
-            score.textContent = result
-            hitPosition = null
-        }
-    })
-})
+class Mole {
+    speed = 1100;
+    wait = 800;
+    timerId;
+    position;
 
-let clicker = 0;
+    constructor() {
 
-let countDownTimerId = 0;
+    }
+}
+
+const game = new Game(9);
+
+const mole = new Mole();
+
+const gridContainer = document.getElementById("gridContainer");
+gridContainer.addEventListener("click", function (event) {
+    game.position = event.target.id;
+    checkWhack();
+});
+
+for (let id = 0; id < game.slots; id++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+
+    div.classList.add('grid-item')
+    img.classList.add('blume');
+
+    img.src = "style/img/blumentopf.png"
+    div.id = id;
+
+    div.appendChild(img);
+    gridContainer.appendChild(div);
+}
+
+const score = document.getElementById('score')
+const timer = document.getElementById('timer')
+reset();
+
+function gameStart() {
+    if (game.started) {
+        return;
+    }
+    game.started = true;
+    moveMole()
+    game.timerId = setInterval(startTimer, 1000)
+}
 
 function moveMole() {
-    clicker++;
-    if (clicker === 1) {
-        countDownTimerId = setInterval(countDown, 1000)
-        timerId = setInterval(randomSquare, milliSecond * 100)
+    mole.timerId = setInterval(changePosition, mole.speed)
+}
+
+function checkWhack() {
+    if (game.position == mole.position) {
+        game.score++
+        score.innerText = game.score
     }
 }
 
-function countDown() {
-    currentTime--
-    timeLeft.textContent = currentTime
+function changePosition() {
+    getRandomPosition()
+    displayMole()
+    setTimeout(removeMole, mole.wait)
+}
 
-    if (currentTime == 0) {
-        clearInterval(countDownTimerId)
-        clearInterval(timerId)
-        alert("Game Over _ Final Score: " + result)
-        clicker = 0;
-        currentTime = 60;
+function getRandomPosition() {
+    const newPosition = Math.floor(Math.random() * game.slots);
+    if (newPosition === 0) {
+        getRandomPosition();
     }
+    else if (mole.position === newPosition) {
+        getRandomPosition();
+    } else {
+        mole.position = newPosition
+    }
+}
+
+function displayMole() {
+    const div = document.getElementById(mole.position)
+    div.classList.add('mole')
+}
+
+function removeMole() {
+    const div = document.getElementById(mole.position)
+    div.classList.remove('mole')
+}
+
+function startTimer() {
+    if (game.count === 0) {
+        gameOver();
+        return
+    }
+    game.count--;
+    timer.innerText = game.count
+}
+
+function gameOver() {
+    clearInterval(game.timerId)
+    clearInterval(mole.timerId)
+    alert("Game Over _ Final Score: " + game.score)
+    reset();
+}
+
+function reset() {
+    timer.innerText = game.time
+    game.count = game.time
+    game.started = false
+    game.score = 0
+    score.innerText = game.score
 }
